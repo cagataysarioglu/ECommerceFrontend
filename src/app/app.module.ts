d04985827a8138776675439573b3758cbc5a6271
Çağatay Sarıoğlu <sarioglucagatay@gmail.com>
2021-03-11 15:27:28 +0300

    Client api access is done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,13 +13,10 @@ import { NavigatorComponent } from './components/navigator/navigator.component';
     AppComponent,
     ProductComponent,
     CategoryComponent,
-    NavigatorComponent
+    NavigatorComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
