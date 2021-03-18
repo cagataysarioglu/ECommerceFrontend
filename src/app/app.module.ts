8971cd55cd76a875d9dbef10989bb1874b7bd64f
Çağatay Sarıoğlu <sarioglucagatay@gmail.com>
2021-03-18 13:06:50 +0300

    Pipe layout is added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NavigatorComponent } from './components/navigator/navigator.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -14,8 +17,9 @@ import { NavigatorComponent } from './components/navigator/navigator.component';
     ProductComponent,
     CategoryComponent,
     NavigatorComponent,
+    VatAddedPipe,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
